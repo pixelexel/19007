0972be5a628b073d75309330028a2283bd642743
Saumitra Bose <imtoobose@gmail.com>
2018-03-28 13:30:54 +0530

    Student

--- frontend/src/components/StudentTable.js
+++ frontend/src/components/StudentTable.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import {
+    Table, TableBody, TableCell, TableFooter, TableHead,
+    TablePagination, TableRow, TableSortLabel,} from 'material-ui'
 
 class StudentTable extends Component{
     render(){
         return(
-        <Table>
-                 
+        <Table>     
         </Table>)
     }
 }
